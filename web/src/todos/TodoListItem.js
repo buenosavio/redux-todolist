fe0986e048c8957d86a637bd76dc0930582d3235
Savio <souzasavio@outlook.com>
2022-06-13 14:15:15 -0300

    feat: included tests with sonin

--- web/src/todos/TodoListItem.js
+++ web/src/todos/TodoListItem.js
@@ -4,6 +4,10 @@ import styled from 'styled-components'
 const ItemContainer = styled.div`
   background: #fff;
   border-radius: 8px;
+  border-bottom: ${props =>
+    new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5)
+      ? 'none'
+      : '2px solid red'};
   margin-top: 8px;
   padding: 16px;
   position: relative;
@@ -42,8 +46,9 @@ const RemovedButton = styled.button`
 `
 
 const TodoListItem = ({ todo, onRemovePressed, onCompletePressed }) => (
-  <ItemContainer>
+  <ItemContainer createdAt={todo.createdAt}>
     <h3>{todo.text}</h3>
+    <p>{`Create at: ${new Date(todo.createdAt).toLocaleDateString()}`}</p>
     <ButtonsContainer>
       {!todo.isCompleted ? (
         <CompletedButton onClick={() => onCompletePressed(todo.id)}>
